ad3b3240f2063233359a4c2891b9085fd3c1d5ab
Martin Kokes <marty.kokes@luminfire.com>
2018-07-27 12:48:40 -0500

    last commit prior to routing

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ import { renderToString } from "react-dom/server"
 import cors from "cors"
 import App from '../shared/App'
 import React from 'react'
+import serialize from "serialize-javascript"
+import { fetchPopularRepos } from '../shared/api'
 
 const app = express()
  
@@ -14,24 +16,27 @@ app.use(cors())
 app.use(express.static("public"))
  
 app.get("*", (req, res, next) => {
-    const markup = renderToString(
-        <App />
-    )
-
-    res.send(`
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>SSR with RR</title>
-            <script src="/bundle.js" defer></script>
-        </head>
-
-        <body>
-            <div id="app">${markup}</div>
-        </body>
-        </html>
-    `)
-})
+    fetchPopularRepos()
+      .then((data) => {
+        const markup = renderToString(
+          <App data={data} />
+        )
+   
+        res.send(`
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <title>SSR with RR</title>
+              <script src="/bundle.js" defer></script>
+              <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+            </head>
+            <body>
+              <div id="app">${markup}</div>
+            </body>
+          </html>
+        `)
+      })
+  })
 
 app.listen(3000, () => {
   console.log(`Server is listening on port: 3000`)
