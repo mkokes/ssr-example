657a7e8586133a33b8ce26e46cfce7ef26516844
Martin Kokes <marty.kokes@luminfire.com>
2018-07-27 13:52:02 -0500

    Example complete

--- src/server/index.js
+++ src/server/index.js
@@ -1,43 +1,57 @@
 import express from "express"
-import { renderToString } from "react-dom/server"
 import cors from "cors"
-import App from '../shared/App'
-import React from 'react'
+import React from "react"
+import { renderToString } from "react-dom/server"
+import { StaticRouter, matchPath } from "react-router-dom"
 import serialize from "serialize-javascript"
-import { fetchPopularRepos } from '../shared/api'
+import App from '../shared/App'
+import routes from '../shared/routes'
 
 const app = express()
- 
+
 app.use(cors())
- 
-// We're going to serve up the public
-// folder since that's where our
-// client bundle.js file will end up.
 app.use(express.static("public"))
- 
+
 app.get("*", (req, res, next) => {
-    fetchPopularRepos()
-      .then((data) => {
-        const markup = renderToString(
-          <App data={data} />
-        )
-   
-        res.send(`
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <title>SSR with RR</title>
-              <script src="/bundle.js" defer></script>
-              <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
-            </head>
-            <body>
-              <div id="app">${markup}</div>
-            </body>
-          </html>
-        `)
-      })
-  })
+  const activeRoute = routes.find((route) => matchPath(req.url, route)) || {}
+
+  const promise = activeRoute.fetchInitialData
+    ? activeRoute.fetchInitialData(req.path)
+    : Promise.resolve()
+
+  promise.then((data) => {
+    const context = { data }
+
+    const markup = renderToString(
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+    )
+
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>SSR with RR</title>
+          <script src="/bundle.js" defer></script>
+          <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+        </head>
+
+        <body>
+          <div id="app">${markup}</div>
+        </body>
+      </html>
+    `)
+  }).catch(next)
+})
 
 app.listen(3000, () => {
   console.log(`Server is listening on port: 3000`)
 })
+
+/*
+  1) Just get shared App rendering to string on server then taking over on client.
+  2) Pass data to <App /> on server. Show diff. Add data to window then pick it up on the client too.
+  3) Instead of static data move to dynamic data (github gists)
+  4) add in routing.
+*/
--- src/shared/Grid.js
+++ src/shared/Grid.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react'
- 
+
 class Grid extends Component {
+  constructor(props) {
+    super(props)
+
+    let repos
+    if (__isBrowser__) {
+      repos = window.__INITIAL_DATA__
+      delete window.__INITIAL_DATA__
+    } else {
+      repos = this.props.staticContext.data
+    }
+
+    this.state = {
+      repos,
+      loading: repos ? false : true,
+    }
+
+    this.fetchRepos = this.fetchRepos.bind(this)
+  }
+  componentDidMount () {
+    if (!this.state.repos) {
+      this.fetchRepos(this.props.match.params.id)
+    }
+  }
+  componentDidUpdate (prevProps, prevState) {
+    if (prevProps.match.params.id !== this.props.match.params.id) {
+      this.fetchRepos(this.props.match.params.id)
+    }
+  }
+  fetchRepos (lang) {
+    this.setState(() => ({
+      loading: true
+    }))
+
+    this.props.fetchInitialData(lang)
+      .then((repos) => this.setState(() => ({
+        repos,
+        loading: false,
+      })))
+  }
   render() {
-    const repos = this.props.data
- 
+    const { loading, repos } = this.state
+
+    if (loading === true) {
+      return <p>LOADING</p>
+    }
+
     return (
       <ul style={{display: 'flex', flexWrap: 'wrap'}}>
         {repos.map(({ name, owner, stargazers_count, html_url }) => (
@@ -19,4 +62,5 @@ class Grid extends Component {
     )
   }
 }
+
 export default Grid
